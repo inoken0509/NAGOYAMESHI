cda527a1158f81237980cf393295435dd3b284e7
Kenta <inoken0509@gmail.com>
2022-10-02 20:59:27 +0900

    テストユーザー追加

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,6 +24,16 @@ class UserSeeder extends Seeder {
         $user->phone_number = '0000000000';
         $user->save();
 
+        $user = new User();
+        $user->name = '侍次郎';
+        $user->email = 'jiro.samurai@example.com';
+        $user->email_verified_at = now();
+        $user->password = Hash::make('password');
+        $user->postal_code = '1010022';
+        $user->address = '東京都千代田区神田練塀町300番地 住友不動産秋葉原駅前ビル5F';
+        $user->phone_number = '0000000000';
+        $user->save();
+
         User::factory()->count(10)->create();
     }
 }
